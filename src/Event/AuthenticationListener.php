f029007fd4218edca5192250eff748447b12ad4b
Robin Delbaere <hello@rdelbaere.fr>
2022-06-25 00:19:26 +0200

    Install & configure basic API

---
+++ src/Event/AuthenticationListener.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Event;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationListener
+{
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $payload = [
+            'status' => true,
+            'data' => $event->getData(),
+        ];
+
+
+        $event->setData($payload);
+    }
+}
